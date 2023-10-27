14224921145072f01c3d3e855cfd1b70fd3b0443
imagine-hussain <hussain.nwz000@gmail.com>
2023-10-28 02:53:57 +1100

    init

--- assets/sw.js
+++ assets/sw.js
@@ -7,18 +7,18 @@ var filesToCache = [
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
+self.ardEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function (response) {
+    caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
     })
   );
